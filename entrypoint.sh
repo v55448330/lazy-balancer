ab0c494b32d80a4713e4323c975dc1dce20cc996
XiaoBao <dyzhang@thoughtworks.com>
2024-07-18 03:03:33 +0800

    upgrade to 1.3.9beta

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/ash
 
-WORKDIR=/app/lazy-balancer
+WORKDIR=/app/lazy_balancer
 
 cd ${WORKDIR}
 
 if [ ! -e "db/db.sqlite3" ]; then
     mkdir -p ${WORKDIR}/db
-    cp -v ${WORKDIR}/db/db.sqlite3.init ${WORKDIR}/db/db.sqlite3
+    cp -v ${WORKDIR}/db.sqlite3.init ${WORKDIR}/db/db.sqlite3
 fi
 
 supervisord -c "/app/lazy_balancer/service/supervisord_docker.conf"
